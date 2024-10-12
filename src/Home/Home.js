dad772176de681d0107ecdbfb3afaea2614a4186
Dongyang7 <spiniklee@gmail.com>
2024-10-11 23:37:01 -0400

    update parent instruction and add child instructions

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -19,31 +19,46 @@ const Home = () => {
 
   return (
     <div className="home">
-      <AppBar className = "titleContainer">
-          <h1 className = "title">
-            {showChineseInstruction ? 
-            <>欢迎来到MERLS</> :
-            <>Welcome to MERLS</>
-            }
-          </h1>
-          <h2 className = "subHeading">
-            {
-              showChineseInstruction ?
-              "请认真观看视频并完成问题再开始测试" :
-              "Please watch the below video and finish the questions before starting tests"
-            }
-          </h2>
-          <button className = "translationButton" onClick ={() => setShowChineseInstruction(!showChineseInstruction)}>
-            {showChineseInstruction ? "Change to English/更改为英语" : "Change to Chinese/更改为中文"}
-          </button>
+      <AppBar className="titleContainer">
+        <h1 className="title">
+          {showChineseInstruction ? <>欢迎来到MERLS</> : <>Welcome to MERLS</>}
+        </h1>
+        <h2 className="subHeading">
+          {showChineseInstruction
+            ? "请认真观看视频并完成问题再开始测试"
+            : "Please watch the below video and finish the questions before starting tests"}
+        </h2>
+        <button
+          className="translationButton"
+          onClick={() => setShowChineseInstruction(!showChineseInstruction)}
+        >
+          {showChineseInstruction
+            ? "Change to English/更改为英语"
+            : "Change to Chinese/更改为中文"}
+        </button>
       </AppBar>
       <div className="introVideo">
         <iframe
           title="parent-instruction"
-          src="https://drive.google.com/file/d/1s-_7BzxT7lKRK6QhR_7cTjiaK7pDQIXZ/preview"
+          src={
+            showChineseInstruction
+              ? "https://drive.google.com/file/d/1L-ndiuh5e-EoFXQwWKn1sNjxAXnMSZc4/preview"
+              : "https://drive.google.com/file/d/1vf9OY__JyOuY7qgM3-GlOskB1NuMzZXE/preview"
+          }
           width="640"
           height="480"
-          allow="autoplay"
+        ></iframe>
+      </div>
+      <div className="introVideo">
+        <iframe
+          title="child-instruction"
+          src={
+            showChineseInstruction
+              ? "https://drive.google.com/file/d/1Iw0x5F4y82vB1HrlF14kujTzffCeostq/preview"
+              : "https://drive.google.com/file/d/1pTGSn4nQEUGCUQfwSY3wZmEQUeviKcyq/preview"
+          }
+          width="640"
+          height="480"
         ></iframe>
       </div>
       {showChineseInstruction ? (
@@ -62,10 +77,7 @@ const Home = () => {
           showChineseInstruction ? "true" : "false"
         }`}
       >
-        {showChineseInstruction ? 
-          <>下一步</> :
-          <>Next</>
-        }
+        {showChineseInstruction ? <>下一步</> : <>Next</>}
       </Button>
     </div>
   );
