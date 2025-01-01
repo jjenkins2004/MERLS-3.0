694bc172c353793c89ac3df4f2c47faae30ed60b
Joshua Jenkins <2004joshuaj@gmail.com>
2024-12-31 16:12:56 -0800

    StoryTest instruction audio play/logic

--- MERLS-3.0/src/StoryTest/StoryTest.js
+++ MERLS-3.0/src/StoryTest/StoryTest.js
@@ -1,4 +1,4 @@
-import { React, useState, useEffect } from "react";
+import { React, useState, useEffect, useRef } from "react";
 import PauseCircleIcon from "@mui/icons-material/PauseCircle";
 import PlayCircleIcon from "@mui/icons-material/PlayCircle";
 import IconButton from "@mui/material/IconButton";
@@ -9,26 +9,66 @@ import CircularProgress from "@mui/material/CircularProgress";
 import { useLocation, useNavigate } from "react-router-dom";
 import "../Tests/Test.scss";
 
+let questionAudio;
+
 const StoryTest = ({ language }) => {
   const [showChinese, setShowChinese] = useState(false);
   const [audioPlaying, setAudioPlaying] = useState(false);
   const [countDown, setCountDown] = useState(3);
-  const [paused, setPaused] = useState(false);
   const [disableOption, setDisableOption] = useState(true);
+  const timeoutRef = useRef(null);
 
   const navigate = useNavigate();
   const location = useLocation();
 
+  //changing language display preference
   useEffect(() => {
     const params = new URLSearchParams(location.search);
     const languageParam = params.get("cn-zw");
     setShowChinese(languageParam === "true");
   }, [location]);
 
+  //initial audio instructions
+  useEffect(() => {
+    clearTimeout(timeoutRef.current);
+    if (countDown > 0) {
+      // timeoutRef is used here so we can pause the countDown by clearing timeout
+      timeoutRef.current = setTimeout(() => {
+        setCountDown((prevCountDown) => prevCountDown - 1);
+      }, 1000);
+    } else {
+      playAudio();
+    }
+
+    return () => clearTimeout(timeoutRef.current);
+  }, [countDown]);
+
+  //function to play instruction/story audio
+  const playAudio = () => {
+    questionAudio = new Audio(
+      "https://sites.usc.edu/heatlab/files/2024/11/RV-English-test-instruction-w-audio.m4a"
+    );
+    questionAudio.addEventListener("play", () => {
+      setAudioPlaying(true);
+    });
+    questionAudio.addEventListener("ended", () => {
+      setAudioPlaying(false);
+
+      if (disableOption) {
+        // after the audio is played for the first time, we will allow user to click the options
+        setDisableOption(false);
+      }
+    });
+    questionAudio.play().catch((error) => {
+      alert("error in playing question.", error);
+      setDisableOption(false);
+    });
+  };
+
   return (
     <div id="testPage">
       <AppBar className="titleContainer">
-      <progress id="progress" value={5} max={10}/>
+        <progress id="progress" value={5} max={10} />
         <TranslationButton
           showChinese={showChinese}
           setShowChinese={setShowChinese}
@@ -44,46 +84,39 @@ const StoryTest = ({ language }) => {
               />
             </IconButton>
             <p className="actionText">
-              {showChinese ? <>播放中</> : <>Playing question</>}
+              {showChinese ? <>播放中</> : <>Playing Instructions</>}
             </p>
           </div>
         ) : (
           <div>
-            {paused ? (
-              <IconButton
-                aria-label="play"
-                style={{ marginBottom: "0" }}
-                onClick={() => {
-                  setPaused(false);
-                  setCountDown(countDown);
-                }}
-              >
-                <PlayCircleIcon color="primary" className={"pauseButton"} />
-              </IconButton>
-            ) : (
-              <IconButton
-                aria-label="pause"
-                style={{ marginBottom: "0" }}
-                onClick={() => {
-                  setPaused(true);
-                }}
-              >
-                <PauseCircleIcon color="primary" className={"pauseButton"} />
-              </IconButton>
-            )}
-            {paused ? (
-              <p className="actionText">
-                {showChinese ? <>已被用户暂停</> : <>Paused by user</>}
-              </p>
-            ) : (
-              <p className="actionText">
-                {showChinese ? (
-                  <>{countDown} 秒内播放音频</>
-                ) : (
-                  <>Audio playing in {countDown} second(s)</>
-                )}
-              </p>
-            )}
+            <IconButton
+              aria-label="play"
+              style={{ marginBottom: "0" }}
+              onClick={() => {
+                playAudio();
+              }}
+            >
+              <PlayCircleIcon color="primary" className={"pauseButton"} />
+            </IconButton>
+            <p className="actionText">
+              {countDown > 0 ? (
+                <p className="actionText">
+                  {showChinese ? (
+                    <>{countDown} 秒内播放音频</>
+                  ) : (
+                    <>Audio playing in {countDown} second(s)</>
+                  )}
+                </p>
+              ) : (
+                <p className="actionText">
+                  {showChinese ? (
+                    <>再听一次指示?</>
+                  ) : (
+                    <>Listen to instructions again?</>
+                  )}
+                </p>
+              )}
+            </p>
           </div>
         )}
       </div>
